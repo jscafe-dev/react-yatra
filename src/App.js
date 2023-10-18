9c75d492703cf54f2cca51ad509ac6653404ea39
Vedant Jain <jscafe.business@gmail.com>
2023-10-18 09:41:09 +0530

    React lazy and Suspense API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, {useState, useMemo, useCallback, useEffect, useRef} from 'react';
+import React, {useState, lazy, Suspense ,useMemo, useCallback, useEffect, useRef} from 'react';
 import Button from './Button'
-import Text from './Text'
+// import Text from './Text'
 import Timer from './Timer'
 import ButtonWithTooltip from './ButtonWithTooltip'
 import Input from './Input'
 import SecondParent from './SecondParent'
 import PrintTable from './PrintTable'
+
+const Text = lazy(() => delayForDemo(import('./Text.js')));
+
+function delayForDemo(promise) {
+    return new Promise(resolve => {
+      setTimeout(resolve, 2000);
+    }).then(() => promise);
+  }
 const App = () => {
-    const [counter1, setCounter1] = useState(0)
-    const [counter2, setCounter2] = useState(0)
-    // const [myObj, setMyObj] = useState({channel: "JS Cafe"})
-    const myObj = useMemo(() => {channel: "JS Cafe"}, [])
-    const arr = useMemo(() => (['a', 'b']), [])
-    const val = 10
+    const [showText, toggleText] = useState(false)
 
     return <>
-        Counter1: {counter1} <button onClick={() => setCounter1((prev) => prev+1)}>count1++</button>
-        <br/>
-        Counter2: {counter2} <button onClick={() => setCounter2((prev) => prev+1)}>count2++</button>
-        <br/>
-        {/* <PrintTable num={counter1} obj={myObj} val={val}/> */}
-        <PrintTable num={counter1} arr={arr} obj={myObj} val={val}/>
+        <button onClick={() => {toggleText((prev) => !prev)}}>Toggle Text</button>
+        
+        {showText && <Suspense fallback={<div>I am loading</div>}>
+            <Text>Hello guys!</Text>
+        </Suspense>}
     </>
 }
 
