b903b340560012aea78d88c980c243825684695c
Vedant Jain <jscafe.business@gmail.com>
2023-10-20 09:40:25 +0530

    Fetch data from API and Image Optimization

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import React from 'react';
-import { Routes, Route, Link, useParams } from 'react-router-dom';
-
-const Home = () => {
-  return <div>I am home</div>
-}
-
-const About = () => {
-  return <div>I am about</div>
-}
-
-const Contact = () => {
-  return <div>I am contact us</div>
-}
-
-const Profile = () => {
-  const {userName} = useParams()
-  return <div>Hello user: {userName}</div>
-}
+import React, {useEffect, useState} from 'react';
 
 const App = () => {
+  const [photos, setPhotos] = useState()
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/photos')
+      const data = await response.json()
+      console.log(data)
+      setPhotos(data)
+    }
 
+    fetchData()
+  }, [])
     return <>
-    <Link to="/">Travel Home</Link> 
-    <Link to="/about">Travel About </Link>
-    <Link to="/contact">Travel Contact </Link>
-    <Link to="/profile/jscafe">Travel Profile </Link>
-    <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/profile/:userName" element={<Profile/>}/>
-    </Routes>
+      <h2>Welcome to gallery</h2>
+
+      {photos && photos?.length ? photos.map((pic) => <img key={pic?.id} loading='lazy' height={200} width={200} src={pic?.url} alt={pic?.title}/>) : <span>Loading Images</span>}
     </>
 }
 
