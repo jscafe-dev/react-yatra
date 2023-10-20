dadd25c87b879f95769bf9605e8f963ff0c306b9
Vedant Jain <jscafe.business@gmail.com>
2023-10-20 11:57:31 +0530

    Styling Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useEffect, useMemo, useState} from 'react';
 import { debounce } from 'lodash';
 
+import styles from './app.module.css'
+import './input.css'
+
 const App = () => {
   const [inputData, setInputData] = useState('')
   const [data, setData] = useState()
@@ -26,7 +29,7 @@ const App = () => {
 
     return <>
       <h2>Welcome to Star Wars</h2>
-      <input type="text" value={inputData} onChange={handleInputChange}/>
+      <input className={styles.inputElement} type="text" value={inputData} onChange={handleInputChange}/>
       <br/>
       {data && data?.length ? <ul>
           {data.map((item) => <li key={item?.created}>{item?.name}</li>)}
